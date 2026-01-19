ef003bdaf79c829fe0a5b71792a2f754de0ccd7e
olkomateusz <mateuszolkobiznes@gmail.com>
2026-01-19 11:05:06 +0100

    dodanie dzielenia

--- kalkulator.c
+++ kalkulator.c
@@ -7,12 +7,6 @@ float dzielenie(float a, float b)
 
 int main()
 {
-    float a;
-    float b;
-    printf("Podaj liczbe a: \n");
-    scanf("%f", &a);
-    printf("Podaj dzielnik b: \n");
-    scanf("%f", &b);
-    printf("Wynik: %f", dzielenie(a,b));
+    
     return 0;
 }
