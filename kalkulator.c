915d7c0cfd344781bb4c3e827a2e5376e7e593f5
olkomateusz <mateuszolkobiznes@gmail.com>
2026-01-19 10:36:29 +0100

    funckja dzielenie

--- kalkulator.c
+++ kalkulator.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+float dzielenie(float a, float b)
+{
+    return a/b;
+}
+
 int main()
 {
-
+    float a;
+    float b;
+    printf("Podaj liczbe a: \n");
+    scanf("%f", &a);
+    printf("Podaj dzielnik b: \n");
+    scanf("%f", &b);
+    printf("Wynik: %f", dzielenie(a,b));
     return 0;
 }
